9b7ee3284dc34eb1813c4442cf773b0d90a021ce
Aysha Rahiman <ayshahadhi2018@gmail.com>
2024-09-23 22:14:11 +0400

    Added checkout testing flow

---
+++ src/main/java/com/aysha/MWCheckOutDataModel.java
@@ -0,0 +1,6 @@
+package com.aysha;
+
+public class MWCheckOutDataModel {
+    String itemSearchKey = "Waterwipes - Plastic Free On The Go Wet Wipes - 28pcs";
+    String productImageKey = ".item-image-2tX";
+}
---
+++ src/main/java/com/aysha/MWConstants.java
@@ -0,0 +1,5 @@
+package com.aysha;
+
+public class MWConstants {
+    static String searchElementKey = "input[placeholder='What are you looking for?']";
+}
